a6468e9142ded10f895f8588a4fc1a716d9c5ab1
Santi <santiagogonzalez@gregoriofer.com>
2018-05-08 20:44:31 +0200

    Ultimas modificaciones del día

--- Cooperativo.ino
+++ Cooperativo.ino
@@ -1,10 +1,10 @@
 #include "Robot.h"
-#include <SimpleTimer.h>
+#include "SimpleTimer.h"
 
 Robot robot;
 
-float angulo = 0f;
-float offset = 5f;
+float angulo = 0.0;
+float offset = 5.0;
 
 SimpleTimer timer;
 int timerId = -1;
@@ -27,7 +27,7 @@ void loop() {
     // comprobarLineasLaterales();
 
     if (abs(anguloRectificacion()) <= offset) { // Esta comprobación es necesaria puesto que solo en caso de que mire en la dirección correcta deberá ignorar la detección de lineas frontalmente
-        comprobarFinRecorrido();
+        //comprobarFinRecorrido();
     } else {
         // comprobarLineasFrontales();
     }
@@ -48,36 +48,41 @@ float anguloRectificacion() {
 
 // Para el robot y lo rota hasta que la dirección es la correcta
 void rectificarAngulo() {
-    float anguloRectificacion = anguloRectificacion();
-    while (abs(anguloRectificacion) > offset) {
-        if (anguloRectificacion > 0) {
-            robot.avanzaDcha(512, 100);
+    float ang = anguloRectificacion();
+    while (abs(ang) > offset) {
+        Serial.print("ar: " + String(ang));
+        if (ang > 0) {
+            robot.avanzaDcha(512, 10);
+            Serial.println(" DERECHA");
         } else {
-            robot.avanzaIzda(512, 100);
+            robot.avanzaIzda(512, 10);
+            Serial.println(" IZQUIERDA");
         }
+        ang = anguloRectificacion();
+            Serial.println("anguloact: " + String(ang));
     }
 }
 
 // array que almacena la detección de la linea de los sensores
-bool[6] sensores = {false, false, false, false, false, false};
+bool sensores[6] = {false, false, false, false, false, false};
 
 // comprueba que se ha detectado una linea a través de todos los sensores por orden
 // en menos de 2 segundos
 void comprobarFinRecorrido() {
     if (!(sensores[2] && sensores[3])) {
-        sensores[2] = robot.irSobreNegro(robot.IR[3]);
-        sensores[3] = robot.irSobreNegro(robot.IR[4]);
+        sensores[2] = robot.irSobreNegro(robot.pins.IR[3]);
+        sensores[3] = robot.irSobreNegro(robot.pins.IR[4]);
 
         if (timerId != -1 && timer.isEnabled(timerId)) {
-            timerId = timer.setTimer(2000, resetFinRecorrido);    
+            timerId = timer.setTimeout(2000, resetFinRecorrido);    
         }
 
     } else if (!(sensores[1] && sensores[4])) {
-        sensores[1] = robot.irSobreNegro(robot.IR[2]);
-        sensores[4] = robot.irSobreNegro(robot.IR[5]);
+        sensores[1] = robot.irSobreNegro(robot.pins.IR[2]);
+        sensores[4] = robot.irSobreNegro(robot.pins.IR[5]);
     } else if (!(sensores[0] && sensores[5])) {
-        sensores[0] = robot.irSobreNegro(robot.IR[0]);
-        sensores[5] = robot.irSobreNegro(robot.IR[7]);
+        sensores[0] = robot.irSobreNegro(robot.pins.IR[0]);
+        sensores[5] = robot.irSobreNegro(robot.pins.IR[7]);
 
         if (sensores[0] && sensores[5]) {
             finalizarRecorido();
